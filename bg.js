3f5ec637a0e99842062afe13f12b67702ec293d3
sena986 <53561380+sena986@users.noreply.github.com>
2020-06-14 01:11:11 +0900

    4. Background Image

---
+++ bg.js
@@ -0,0 +1,19 @@
+const body = document.querySelector("body");
+
+function handleImgLoad() {
+  console.log("hello");
+}
+
+function paintImage() {
+  const image = new Image();
+  image.src = `https://images.unsplash.com/photo-1591749161298-e4af15ec787d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60`;
+  body.appendChild(image);
+  image.classList.add("bg");
+  image.addEventListener("load", handleImgLoad);
+}
+
+function init() {
+  paintImage();
+}
+
+init();
